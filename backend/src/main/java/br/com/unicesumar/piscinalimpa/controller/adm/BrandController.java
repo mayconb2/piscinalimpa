ec32c352bfda54ac268c37323ef3cdb9d339c6b7
Maycon de Souza <mayconb2@gmail.com>
2022-04-21 21:56:19 +0100

    Added brand classes

---
+++ backend/src/main/java/br/com/unicesumar/piscinalimpa/controller/adm/BrandController.java
@@ -0,0 +1,38 @@
+package br.com.unicesumar.piscinalimpa.controller.adm;
+
+import br.com.unicesumar.piscinalimpa.dto.BrandDTO;
+import br.com.unicesumar.piscinalimpa.service.BrandService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/adm/v1/brand")
+@Slf4j
+public class BrandController {
+
+    private final BrandService brandService;
+
+    public BrandController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Bearer Token Needed", authorizations = { @Authorization(value="jwtToken") })
+    public ResponseEntity<List<BrandDTO>> findAll() {
+        try {
+            List<BrandDTO> brandDTOS = brandService.findAll();
+            return ResponseEntity.ok(brandDTOS);
+        } catch (Exception e) {
+            log.error("Erro ao consultar lista de marcas {}", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
--- backend/src/main/java/br/com/unicesumar/piscinalimpa/service/BrandService.java
+++ backend/src/main/java/br/com/unicesumar/piscinalimpa/service/BrandService.java
@@ -1,21 +1,34 @@
 package br.com.unicesumar.piscinalimpa.service;
 
+import br.com.unicesumar.piscinalimpa.dto.BrandDTO;
 import br.com.unicesumar.piscinalimpa.entity.Brand;
 import br.com.unicesumar.piscinalimpa.exception.NotFoundException;
 import br.com.unicesumar.piscinalimpa.repository.BrandRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BrandService {
 
     private final BrandRepository brandRepository;
+    private final ModelMapper mapper;
 
-    public BrandService(BrandRepository brandRepository) {
+    public BrandService(BrandRepository brandRepository, ModelMapper mapper) {
         this.brandRepository = brandRepository;
+        this.mapper = mapper;
     }
 
     public Brand findById(Long id) {
         return this.brandRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Não foi possível encontrar marca com id: " + id));
+    }
+
+    public List<BrandDTO> findAll() {
+         return this.brandRepository.findAll().stream()
+                 .map(brand -> mapper.map(brand, BrandDTO.class))
+                 .collect(Collectors.toList());
     }
 }
